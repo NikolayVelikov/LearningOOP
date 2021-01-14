254c43c8a2aff58b0c90aa6ea65a229e11578b28
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-01-14 23:27:08 +0200

    Laboratory Enheritance

---
+++ LAB Inheritance/CustomRandomList/RadomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+   public class RadomList : List<string>
+    {
+
+        private Random rand;
+
+        public RadomList()
+        {
+           rand = new Random();
+        }
+        public string RemoveRandomElement()
+        {
+            int index = rand.Next(0, this.Count);
+            string remove = this[index];
+            this.RemoveAt(index);
+            return remove;
+        }
+    }
+}
---
+++ LAB Inheritance/CustomRandomList/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RadomList list = new RadomList();
+            list.Add("gosho");
+            list.Add("petar");
+            list.Add("ivan");
+            
+            Console.WriteLine(list.RemoveRandomElement());
+        }
+    }
+}
